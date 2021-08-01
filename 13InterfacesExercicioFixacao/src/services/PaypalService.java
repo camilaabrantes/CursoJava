61443eed7ec2887cc5a6dfdeca8c3e4253562a29
Camila <camila.abrantes@ee.ufcg.edu.br>
2021-08-01 19:18:47 -0300

    Commit 16: Ajustes Interface

--- 13InterfacesExercicioFixacao/src/services/PaypalService.java
+++ 13InterfacesExercicioFixacao/src/services/PaypalService.java
@@ -2,9 +2,6 @@ package services;
 
 public class PaypalService implements OnlinePaymentServices{
 
-	public Double testando() {
-		return 2.00;
-	}
 	@Override
 	public Double interest(Double amount, Integer months) {
 		Double parcelaInterest = amount + 0.01*amount*months;		
